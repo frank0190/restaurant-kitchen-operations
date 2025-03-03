d573285bd691c6757ef68e8c39eadb3c133ba28a
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-02 21:32:53 -0600

    Create new models and update application configuration

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,41 @@
+from decimal import Decimal
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
+    dish_type = models.ForeignKey(DishType, related_name="dishes", on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name='cooked_dishes')
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
--- restaurant_kitchen_operations/wsgi.py
+++ restaurant_kitchen_operations/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_kitchen_operations.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "restaurant_kitchen_operations.settings"
+)
 
 application = get_wsgi_application()
