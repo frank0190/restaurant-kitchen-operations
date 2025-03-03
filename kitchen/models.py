4af8d3c1871c11e3f2c8ec2b882dcadf42235ec3
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-02 21:41:03 -0600

    Update Dish model to use AUTH_USER_MODEL for cooks

--- kitchen/models.py
+++ kitchen/models.py
@@ -3,6 +3,8 @@ from django.core.validators import MinValueValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from restaurant_kitchen_operations import settings
+
 
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -32,7 +34,7 @@ class Dish(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     dish_type = models.ForeignKey(DishType, related_name="dishes", on_delete=models.CASCADE)
-    cooks = models.ManyToManyField(Cook, related_name='cooked_dishes')
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='cooked_dishes')
 
     class Meta:
         ordering = ["name"]
