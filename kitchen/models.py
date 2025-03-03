3e365dce9629a131333decd69d6ae3ada4600850
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-02 21:49:49 -0600

    Update models (Dish, Cook, DishType) and apply migrations

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,9 +1,8 @@
 from decimal import Decimal
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-from restaurant_kitchen_operations import settings
 
 
 class DishType(models.Model):
