60db51a621bc2a6ad5e0660466d52a64cae44f1c
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-04 12:22:20 -0600

    Add index view to display dish types, dishes, and cooks with visit counter

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import DishType, Dish, Cook
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_dish_types = DishType.objects.all()
+    num_dishes = Dish.objects.all()
+    num_cooks = Cook.objects.all()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+        "num_visits": num_visits
+    }
+    return render(request, "index.html", context=context)
