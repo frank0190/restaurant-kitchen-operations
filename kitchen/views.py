7b626b6c63f418f01746550e962c44f4b81a5a21
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-04 22:35:37 -0600

    Create ListViews for models (Cook, Dish, DishType) and corresponding templates

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
+from django.views import generic
 from kitchen.models import DishType, Dish, Cook
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    num_dish_types = DishType.objects.all()
-    num_dishes = Dish.objects.all()
-    num_cooks = Cook.objects.all()
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
 
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
@@ -18,4 +18,18 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_cooks": num_cooks,
         "num_visits": num_visits
     }
-    return render(request, "index.html", context=context)
+    return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type"
+
+
+class DishListView(generic.ListView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
