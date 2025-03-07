244c32ca3b9771eaa2dbf4c545c00c2821fd32d4
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-06 21:29:06 -0600

    Add create views for DishType, Dish, and Cook models

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+
+from kitchen.forms import CookCreationForm
 from kitchen.models import DishType, Dish, Cook
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_dish_types = DishType.objects.count()
     num_dishes = Dish.objects.count()
@@ -21,28 +27,45 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "kitchen/index.html", context=context)
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     template_name = "kitchen/dish_type_list.html"
     context_object_name = "dish_type_list"
     paginate_by = 5
 
 
-class DishListView(generic.ListView):
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+    template_name = "kitchen/dish_type_form.html"
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     queryset = Dish.objects.select_related("dish_type")
     paginate_by = 5
 
 
-class DishDetailView(generic.DetailView):
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-list")
+    template_name = "kitchen/dish_form.html"
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     paginate_by = 5
 
 
-class CookDetailView(generic.DetailView):
+class CookCreateView(LoginRequiredMixin, generic.CreateView):
     model = Cook
+    form_class = CookCreationForm
+    success_url = reverse_lazy("kitchen:cook-list")
 
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cook
