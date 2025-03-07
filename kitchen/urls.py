68541475bee013affd010827d8dfd275b528548a
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-06 22:14:13 -0600

    Added new paths for updating dish types and dishes in urls.py

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from kitchen.views import index, DishTypeListView, DishListView, CookListView, DishDetailView, CookDetailView, \
-       DishTypeCreateView, DishCreateView, CookCreateView
+       DishTypeCreateView, DishCreateView, CookCreateView, DishTypeUpdateView, DishUpdateView
 
 urlpatterns = [
        path("", index, name="index"),
        path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
        path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
+       path("dish-types/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
        path("dishes/", DishListView.as_view(), name="dish-list"),
        path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
        path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+       path("dishes/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
        path("cooks/", CookListView.as_view(), name="cook-list"),
        path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
        path("cooks/create", CookCreateView.as_view(), name="cook-create"),
