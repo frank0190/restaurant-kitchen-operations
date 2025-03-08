2e346120e04e49b3e3161a478d3129f96ef7d883
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-07 23:45:56 -0600

    Created DishForm to allow selection of multiple users (cooks) for dishes

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from kitchen.models import Cook
+from django import forms
+
+from kitchen.models import Cook, Dish
 
 
 class CookCreationForm(UserCreationForm):
@@ -11,3 +14,14 @@ class CookCreationForm(UserCreationForm):
             "last_name",
             "years_of_experience",
         )
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
