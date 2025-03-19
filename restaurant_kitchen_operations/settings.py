ddf74a99c231d5aa8c457deda90d47636904611a
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-18 21:52:58 -0500

    Added context processor 'cfg_assets_root' to template for asset root path

--- restaurant_kitchen_operations/settings.py
+++ restaurant_kitchen_operations/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "kitchen.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -125,6 +126,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
